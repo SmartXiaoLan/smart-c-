09e3bb116bd4391f8f82d8b4db24368d3793fdc8
Smart-T <shaoteemo@qq.com>
2020-08-20 16:45:27 +0800

    First C# Code Test

---
+++ second_project/BaseGrammar/BaseGrammar/BaseGrammar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BaseGrammar
+{
+    class BaseGrammar
+    {
+        //成员变量
+        private double _length;
+
+        private double _width;
+
+        public void AcceptDetails()
+        {
+            _length = 4.5;
+            _width = 3.5;
+        }
+
+        private double GetArea()
+        {
+            return _length * _width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length:{0}",_length);
+            Console.WriteLine($"Length:{_width}");
+            Console.WriteLine("Length:{0}",GetArea());
+        }
+    }
+
+    class ExecuteRectangle
+    {
+        private static void Main(string[] args)
+        {
+            var baseGrammar = new BaseGrammar();
+            baseGrammar.AcceptDetails();
+            baseGrammar.Display();
+        }
+    }
+}
---
+++ second_project/ConsoleApplication/HelloWorld/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            /* 我的第一个 C# 程序 */
+            Console.WriteLine("Hello World!");
+            // Console.ReadKey();
+        }
+    }
+}
